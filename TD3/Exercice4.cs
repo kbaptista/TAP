203b26f11cac40f44e13906beeb132da9ae1a822
Kevin Baptista <kbaptist@emi.u-bordeaux1.fr>
2016-10-22 11:56:19 +0200

    TD3-Exercice4 PAS FINIS

--- TD3/Exercice4.cs
+++ TD3/Exercice4.cs
@@ -1,16 +1,27 @@
+using System;
 using System.Collections.Generic;
 
-// Create an association list
-List<KeyValuePair <int, string>> list = new List<KeyValuePair <int, string>>();
+class Program{
 
-// Fill it with interesting values
-list.Add(new KeyValuePair<int, string>(1,"un"));
-list.Add(new KeyValuePair<int, string>(7,"sept"));
-list.Add(new KeyValuePair<int, string>(1,"uno"));
+	/***** Map ? *****/
+	public delegate void Map<T1, T2>(List<KeyValuePair<T1, T2>> l, Func<T2,T2> f);
 
-// Get a collection of the keys (names). 
-List<int> keys = Map(list, i => i.Key);
+	public static void Main(){
+		// Create an association list
+		List<KeyValuePair <int, string>> list = new List<KeyValuePair <int, string>>();
 
-// Print the values
-foreach (int k in keys)
-	Console.WriteLine("{0} : {1}", k, list.Find( i => (i.Key == k)).Value);  
+		// Fill it with interesting values
+		list.Add(new KeyValuePair<int, string>(1,"un"));
+		list.Add(new KeyValuePair<int, string>(7,"sept"));
+		list.Add(new KeyValuePair<int, string>(1,"uno"));
+
+
+		// Get a collection of the keys (names). 
+		List<int> keys = Map<int,string>(list, i => i.Key);
+
+		// Print the values
+		//foreach (int k in keys)
+		//	Console.WriteLine("{0} : {1}", k, list.Find( i => (i.Key == k)).Value);  
+
+
+	}
