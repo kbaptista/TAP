9646cfaf53bc12b9f33421525ab510b2da07cb64
Nathalie Craeye <nathalie.craeye@etu.u-bordeaux.fr>
2016-10-26 04:22:21 +0200

    TD3 fini

--- TD3/Exercice1.ml
+++ TD3/Exercice1.ml
@@ -1,9 +1,11 @@
+(*** Question 1 ***)
 
-(*** question 1 ***)
 let func (x, y, z) = y ;;
 func (2, "a", 1.0);;
 
-(*** question 2 ***)
+
+(*** Question 2 ***)
+
 let plus (x,y) = x+y;;
 let fois x y = x*y;;
 let curryf f x y = f (x, y) ;;
@@ -15,13 +17,37 @@ let plus_curryfie = curryf plus;;
 plus_curryfie 2 3;;
 
 
-(*** question 3 ***)
+(*** Question 3 ***)
+
 let diviser x y = x/y;;
 let inverser_argument f x y = f y x;;
 
 let x = diviser 2 3;;
 let y = inverser_argument diviser 2 3;;
 
-(* intzeret : attendre un des arguments qui serait passé *)
+(* L'intérêt de l'inversion des paramètres est de pouvoir traiter simplement des exceptions survenues juste à cause d'un mauvais placement des paramètres *)
 
-(*** question 4 ***)
+
+(*** Question 4 ***)
+
+let compose f g = function x -> f (g x);;
+(* val compose : ('a -> 'b) -> ('c -> 'a) -> 'c -> 'b = <fun> *)
+let rec iterate f n = 
+  if n = 0 then function x -> x 
+  else compose f (iterate f (n - 1));;
+(* val iterate : ('a -> 'a) -> int -> 'a -> 'a = <fun> *)
+
+(* exemple qui a servi de démarche pour la construction de la fonction iterate : *)
+let add_un x = x+1;;
+let un = add_un 0;;
+let deux = add_un un;;
+let trois = add_un deux;;
+let troisBis = add_un (add_un (add_un 0));;
+let troisAvecFct f = f (f (f 0));;
+(* val valTroisAvecFct : int = 3 *)
+
+let valTroisAvecFct = troisAvecFct add_un;;
+(* val valTroisAvecFct : int = 3 *)
+
+let valeurTroisAvecIterate = iterate add_un 3 0;;
+(* val valeurTroisAvecIterate : int = 3 *)
--- TD3/Exercice2.ml
+++ TD3/Exercice2.ml
@@ -1,17 +1,24 @@
-(*** question 1 ***)
-[];; (* # - : 'a list = [] ,, généricité et atomicité du module List *)
-2::1::[];; (* - : int list = [1] ,, détermination du type des valeurs dans la liste *)
-[1; 2];;
-(* [(1, "truc");(2,"bidule")] : clef int, valeur string *)
+(*** Question 1 ***)
 
+[];; (* # - : 'a list = [] , généricité et atomicité du module List *)
+2::1::[];; (* - : int list =  [2; 1] , détermination du type des valeurs car en OCaml les listes sont homogènes *)
+[1; 2];; (* - : int list = [1; 2] *)
+[(1, "truc");(2,"bidule")];;
+(* couples composés d'une clef de type int et d'une valeur de type string : 
+   (int * string) list = [(1, "truc"); (2, "bidule")] *)
 
 let rec voir l =
-   match l with
-     | [] -> print_int 0
-     | t::q->print_int t;  voir q;;
+  match l with
+    | [] -> ()
+    | t::q -> print_int t; print_string " "; voir q;;
+(* val voir : int list -> unit = <fun> *)
+
+let maListe = [1;2;3];; (* val maListe : int list = [1; 2; 3] *)
+voir maListe;; (* 1 2 3 - : unit = () *)
 
 
-(*** question 2 ***)
+(*** Question 2 ***)
+
 let rec list_remove list key =
   let getKey (a,b)=a in
   match list with
@@ -19,19 +26,40 @@ let rec list_remove list key =
     | t::q->if(getKey(t)=key) then
 	list_remove q key else
 	t::list_remove q key;;
-let li = (1,"o")::(2,"t")::(3,"g")::[];;
-list_remove li 1;;
+(* val list_remove : ('a * 'b) list -> 'a -> ('a * 'b) list = <fun> *)
 
-(*** question 3 ***)
-let add x = x+x;;
-let l = 2::1::3::[];;
+let li = (1,"o")::(2,"t")::(3,"g")::[];;
+list_remove li 1;; (* - : (int * string) list = [(2, "t"); (3, "g")] *)
+
+
+(*** Question 3 ***)
+
+(* OCaml est implicitement générique. Du coup la fonction ci-dessous fonctionne 
+   pour n'importe que type de liste *)
 let rec list_map list fonction = 
   match list with
     | [] -> []
     | t::q -> (fonction t)::list_map q fonction;;
-list_map l add;;
 
-(*** question 4 ***)
-let l1 = 1::2::3::[];;
-let l2 = 4::5::6::[];;
-let rec list_concat list1 list2 = 
+let doubler x = x+x;;
+let l = 2::1::3::[];;
+list_map l doubler;; (* - : int list = [4; 2; 6] *)
+
+let displayKey (x,y) = print_int x; print_string " ";;
+let lAssoc = [(1, "truc");(2,"bidule")];;
+list_map lAssoc displayKey;;  (* 2 1 - : unit list = [(); ()] *)
+
+
+(*** Question 4 ***)
+
+let rec lists_concat list1 list2 =
+  match list1 with
+    | [] -> list1@list2 (* recursif terminal *)
+    | t::q -> t::lists_concat q list2;;
+(* val lists_concat : 'a list -> 'a list -> 'a list = <fun> *)
+
+let listeAssoc1 = [(1, "truc");(2,"bidule")];;
+let listeAssoc2 = [(3, "truc");(4,"bidule")];;
+lists_concat listeAssoc1 listeAssoc2;;
+(* - : (int * string) list =
+[(1, "truc"); (2, "bidule"); (3, "truc"); (4, "bidule")] *)
