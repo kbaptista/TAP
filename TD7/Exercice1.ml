dc88e9aa04c1082c26a3b63009c8e984ac4fe077
Nathalie Craeye <nathalie.craeye@etu.u-bordeaux.fr>
2016-11-24 14:24:09 +0100

    Qeustion 4 ok

--- TD7/Exercice1.ml
+++ TD7/Exercice1.ml
@@ -7,9 +7,28 @@ type 'k key = K of 'k
 and ('k, 'a) vertex = 'k key * 'a 
 and ('k, 'a) graph = Graph of (('k, 'a) vertex * 'k key list) list;;
  
-Graph ([
+let mon_graphe = Graph ([
   ((K 1, "Sapristi"), [K 2;K 3;K 4]);
   ((K 2, "Saperlotte"), [K 1;K 3]);
   ((K 3, "Sacrebleu"), [K 1;K 2]);
   ((K 4, "Sacristin"), [K 1]);
-       ])
+       ]);;
+
+
+(* Question 1 *)
+  
+
+(* Question 2 *)
+  
+
+(* Question 3 *)
+
+  
+(* Question 4 *)
+  
+let graph_nexts(Graph g) som =
+  List.assoc som (List.map (fun ((k, _),n) -> (k, n)) g);;
+(* val graph_nexts : ('a, 'b) graph -> 'a key -> 'a key list = <fun> *)
+
+graph_nexts mon_graphe (K 1);;
+(* - : int key list = [K 2; K 3; K 4] *)
