134493699d2a76733c04d5372776955cec2fccd9
Nathalie Craeye <nathalie.craeye@etu.u-bordeaux.fr>
2016-11-24 15:21:25 +0100

    Exo 3 - question 1 ok

---
+++ TD7/Exercice3.ml
@@ -0,0 +1,25 @@
+(***** Exercice 3 *****)
+
+type 'a set = Set of ('a -> bool);;
+
+
+(* Question 1 *)
+
+let set_empty = Set (fun x -> false);;
+let set_full = Set (fun x -> true);;
+let singleton x = Set (fun y -> x=y);;
+
+
+(* Question 2 *)
+
+
+(* Question 3 *)
+
+
+(* Question 4 *)
+
+let set_even = Set (fun x -> (x mod 2) == 0);;
+let set_contains_0 (Set s) = s 0;;  (* pattern-matching *)
+ 
+let ( +| ) (Set s1) (Set s2) = Set (fun y -> s1 y || s2 y);;  
+let ( *| ) (Set s1) (Set s2) = Set (fun y -> s1 y && s2 y);; 
