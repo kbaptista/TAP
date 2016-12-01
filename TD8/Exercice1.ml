e1adae5561abf52e43c2af9426f9d719d49bc02e
Kevin Baptista <kbaptist@emi.u-bordeaux1.fr>
2016-12-01 13:40:36 +0100

    Exercice1.ml

---
+++ TD8/Exercice1.ml
@@ -0,0 +1,17 @@
+(*** Exercice  1  ***)
+ 
+type 'a frozen_flow = 
+| End of 'a
+| Step of (unit -> 'a frozen_flow);;
+ 
+let ppcm x y =
+  let rec ppcm_rec x y mul = 
+    if (y > x) then (ppcm_rec y x mul) else (* Ensures x >= y *)
+    if (x = 0) then 0 else                  (* Ensures both are positive *)
+      let r = (x mod y) in 
+        if (r = 0) then (mul/y) else ppcm_rec y r mul
+  in ppcm_rec x y (x*y);;
+ 
+type ('key,'data) frozen_data_flow = 
+| End of 'data
+| Step of (unit -> (('key*'data) list) * ('key,'data) frozen_data_flow);;
