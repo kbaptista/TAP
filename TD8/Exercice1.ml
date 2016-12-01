b007e559e1acf47b7f4590960e8549104c3efd64
Kevin Baptista <kbaptist@emi.u-bordeaux1.fr>
2016-12-01 14:22:20 +0100

    question 1

--- TD8/Exercice1.ml
+++ TD8/Exercice1.ml
@@ -1,5 +1,5 @@
-(*** Exercice  1  ***)
- 
+(*** Question  1  ***)
+
 type 'a frozen_flow = 
 | End of 'a
 | Step of (unit -> 'a frozen_flow);;
@@ -9,14 +9,15 @@ let thaw input =
     | End x -> End x
     | Step f -> f ();; (* () est le type de base, analogue à null en c *)
 
+(*** Question 2 ***)
 
 let ppcm x y =
   let rec ppcm_rec x y mul = 
-    if (y > x) then (ppcm_rec y x mul) else (* Ensures x >= y *)
-    if (x = 0) then 0 else                  (* Ensures both are positive *)
+    if (y > x) then Step (fun () -> ppcm_rec y x mul) else (* Ensures x >= y *)
+    if (x = 0) then End 0 else                  (* Ensures both are positive *)
       let r = (x mod y) in 
-        if (r = 0) then (mul/y) else ppcm_rec y r mul
-  in ppcm_rec x y (x*y);;
+        if (r = 0) then End (mul/y) else Step (fun () -> ppcm_rec y r mul)
+  in  ppcm_rec x y (x*y);;
  
 type ('key,'data) frozen_data_flow = 
 | End of 'data
